e6d865fa50e2df41164801adb0874a40c34c615d
Makena <43780622+MakenaEurydice@users.noreply.github.com>
2022-09-22 21:53:42 +0300

    comments

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import random
 from art import stages
 
+# select a random word
 chosen_word = random.choice(open("words.txt").read().split())
 word_length = len(chosen_word)
-
-end_of_game = False
-lives = 6
 
 display = []
 for i in range(word_length):
     display += '_'
 
+end_of_game = False
+lives = 6
+
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     if guess in display:
-        print("Letter already guessed")
+        print("Already guessed.Try another letter")
 
     for position in range(word_length):
         letter = chosen_word[position]
@@ -23,13 +24,15 @@ while not end_of_game:
         if letter == guess:
             display[position] = letter
 
+    # Let user know if guessed letter is not in the word
     if guess not in chosen_word:
         print("Letter you've guessed is not in the word.You lose a life")
         lives -= 1
         if lives == 0:
             end_of_game = True
-            print("Oops,you lose")
+            print(f"Oops,you lose.The word was {chosen_word}")
 
+    # Print elements in the list as a string
     print(f"{' '.join(display)}")
 
     if "_" not in display:
